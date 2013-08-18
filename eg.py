d757f28ec9726cb8d0a2758fb6877bd82e506eb9
Amjad Masad <amjad.masad@gmail.com>
2013-08-18 03:34:29 -0400

    move examples

---
+++ eg.py
@@ -0,0 +1,7 @@
+from arabish import transliterate
+
+print transliterate('thahab alwalado 2la almadrasa lyata3alam allo3\'ah al3arabieh belenglizieh')
+print transliterate('matha taf3alo ya fata')
+print transliterate('qlto laka 2thhab men hona 2mok tab7atho 3ank')
+print transliterate('talawathat almiah')
+print transliterate('sho betgool')
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='arabish',
+    version='0.1dev',
+    packages=['arabish'],
+    long_description=open('README.md').read(),
+)
