c9cfb39b814a875f9348bd9b8127d56bc51cc3ef
Amjad Masad <amjad.masad@gmail.com>
2013-08-18 02:43:52 -0400

    verbose

--- transliterate.py
+++ transliterate.py
@@ -88,15 +88,20 @@ def transliterate_word(english):
   recur(english, '')
   return ret
 
-def transliterate(sentence):
+def transliterate(sentence, verbose=False):
   words = sentence.split()
   ret = []
   for word in words:
     candidates = list(transliterate_word(word))
+    if verbose:
+      for word in candidates:
+        print word
     ret.append(sort_by_frequency(candidates)[0])
   return ' '.join(ret)
 
 print transliterate('thahab alwalado 2la almadrasa lyata3alam allo3\'ah al3arabieh belenglizieh')
 print transliterate('matha taf3alo ya fata')
 print transliterate('qlto laka 2thhab men hona 2mok tab7atho 3ank')
+print transliterate('talawathat almiah')
+print transliterate('sho betgool')
 
